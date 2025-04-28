1bc6b053ba89fe6ec7683a7c988aa292a7a2c1e9
DwiSetiawati <dwisetwa27@gmail.com>
2025-04-28 11:06:40 +0700

    membuat input data

--- class.cpp
+++ class.cpp
@@ -9,4 +9,15 @@ public:
 
 private:
     int harga_mobil;
+
+public:
+    void input_data()
+    {
+        cout << "Masukkan nama mobil: ";
+        cin >> nama_mobil;
+        cout << "Masukkan tipe mobil: ";
+        cin >> tipe_mobil;
+        cout << "Masukkan harga mobil: ";
+        cin >> harga_mobil;
+    }
 };
